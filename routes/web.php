f71a97fb918ef6df8052d9b29ed8dcff332d9ad8
nurfaiz26 <mnurfaiz26@gmail.com>
2024-10-04 21:21:37 +0700

    learn laravel part 4

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,35 @@ Route::get('/jobs', function () {
     // with pagination
     // $jobs = Job::with('employer')->paginate(3);
     // $jobs = Job::with('employer')->simplePaginate(3);
-    $jobs = Job::with('employer')->cursorPaginate(3);
+    $jobs = Job::with('employer')->latest()->cursorPaginate(3);
 
 
-    return view('jobs', [
+    return view('jobs.index', [
         "jobs" => $jobs,
 
     ]);
     // return ["foo" => "boo"];
+});
+
+Route::post('/jobs', function() {
+    // dd(request()->all());
+
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+
+    Job::create([
+       'title' => request('title'),
+       'salary' => request('salary'),
+       'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
@@ -35,7 +56,7 @@ Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find($id);
 
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
